d6f94c6be5d5370a988fb15f6bf1bf62c7fe1e21
Ruz-len <84587660+Ruz-len@users.noreply.github.com>
2021-06-13 20:14:19 +0300

    adaptive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,23 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    spaceBetween: 50,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
     autoplay: {
         delay: 5000,
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 3
+        },
+        992: {
+            slidesPerView: 4
+        },
+        1200: {
+          spaceBetween: 50
+        }
     }
 });
